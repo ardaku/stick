6f0f2250498b5932f7c55919ce23318599921989
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-11-25 15:26:59 -0600

    Rename controllers identifier in example to ctlrs

--- src/lib.rs
+++ src/lib.rs
@@ -26,24 +26,24 @@
 //!
 //! async fn event_loop() {
 //!     let mut listener = Controller::listener();
-//!     let mut controllers = Vec::<Controller>::new();
+//!     let mut ctlrs = Vec::<Controller>::new();
 //!     'e: loop {
-//!         match poll![listener, poll!(controllers)].await.1 {
+//!         match poll![listener, poll!(ctlrs)].await.1 {
 //!             (_, Event::Connect(new)) => {
 //!                 println!(
 //!                     "Connected p{}, id: {:04X}_{:04X}_{:04X}_{:04X}, name: {}",
-//!                     controllers.len() + 1,
+//!                     ctlrs.len() + 1,
 //!                     new.id()[0],
 //!                     new.id()[1],
 //!                     new.id()[2],
 //!                     new.id()[3],
 //!                     new.name(),
 //!                 );
-//!                 controllers.push(*new);
+//!                 ctlrs.push(*new);
 //!             }
 //!             (id, Event::Disconnect) => {
 //!                 println!("Disconnected p{}", id + 1);
-//!                 controllers.swap_remove(id);
+//!                 ctlrs.swap_remove(id);
 //!             }
 //!             (id, Event::Home(true)) => {
 //!                 println!("p{} ended the session", id + 1);
@@ -53,10 +53,10 @@
 //!                 println!("p{}: {}", id + 1, event);
 //!                 match event {
 //!                     Event::ActionA(pressed) => {
-//!                         controllers[id].rumble(if pressed { 1.0 } else { 0.0 });
+//!                         ctlrs[id].rumble(if pressed { 1.0 } else { 0.0 });
 //!                     }
 //!                     Event::ActionB(pressed) => {
-//!                         controllers[id].rumble(if pressed { 0.3 } else { 0.0 });
+//!                         ctlrs[id].rumble(if pressed { 0.3 } else { 0.0 });
 //!                     }
 //!                     _ => {}
 //!                 }
