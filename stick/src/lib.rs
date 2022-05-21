fad61909d147e67fe602b0fbe3e56ea46ab8bc7d
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-05-21 15:15:44 -0500

    Fix tests

--- stick/src/lib.rs
+++ stick/src/lib.rs
@@ -14,12 +14,12 @@
 //! ```rust,no_run
 //! use pasts::Loop;
 //! use std::task::Poll::{self, Pending, Ready};
-//! use stick::{Controller, Event, Listener};
+//! use stick::{Controller, Event, Connector};
 //!
 //! type Exit = usize;
 //!
 //! struct State {
-//!     listener: Listener,
+//!     connector: Connector,
 //!     controllers: Vec<Controller>,
 //!     rumble: (f32, f32),
 //! }
@@ -66,13 +66,13 @@
 //!
 //! async fn event_loop() {
 //!     let mut state = State {
-//!         listener: Listener::default(),
+//!         connector: Connector::default(),
 //!         controllers: Vec::new(),
 //!         rumble: (0.0, 0.0),
 //!     };
 //!
 //!     let player_id = Loop::new(&mut state)
-//!         .when(|s| &mut s.listener, State::connect)
+//!         .when(|s| &mut s.connector, State::connect)
 //!         .poll(|s| &mut s.controllers, State::event)
 //!         .await;
 //!
