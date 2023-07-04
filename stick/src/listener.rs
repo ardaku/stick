a67442fae116075633b6aa0e2cff0deea28f2f2d
Jeron Aldaron Lau <aldaronlau@gmail.com>
2023-07-04 13:58:34 -0500

    Remove remaining each-file copyright notices

--- stick/src/listener.rs
+++ stick/src/listener.rs
@@ -1,14 +1,3 @@
-// Stick
-// Copyright Jeron Aldaron Lau 2021.
-//
-// Licensed under any of:
-// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
-// - MIT License (https://mit-license.org/)
-// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
-// At your option (See accompanying files LICENSE_APACHE_2_0.txt,
-// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).  This file may not be copied,
-// modified, or distributed except according to those terms.
-
 use std::{
     fmt::Debug,
     future::Future,
--- stick/src/raw/windows.rs
+++ stick/src/raw/windows.rs
@@ -1,15 +1,3 @@
-// Stick
-// Copyright Daniel Parnell 2021.
-// Copyright Jeron Aldaron Lau 2021.
-//
-// Licensed under any of:
-// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
-// - MIT License (https://mit-license.org/)
-// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
-// At your option (See accompanying files LICENSE_APACHE_2_0.txt,
-// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).  This file may not be copied,
-// modified, or distributed except according to those terms.
-
 //! This file's code is based on https://github.com/Lokathor/rusty-xinput
 
 use std::{
